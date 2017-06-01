8db7492554e98b38578b6ab3d91a2e843af8eb00
Saki Sato <saki@rayogram.com>
2017-06-01 15:35:14 -0400

    google analytics

--- wp-content/themes/shinbudokai/header.php
+++ wp-content/themes/shinbudokai/header.php
@@ -42,6 +42,19 @@ $template = get_template_directory_uri();
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-100369433-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'shinbudokai' ); ?></a>
 
