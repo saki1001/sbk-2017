8a3b77eb7b714df05f73516eba5068ca81d65f26
Saki Sato <saki@rayogram.com>
2020-04-05 02:00:56 -0400

    add custom sidebar

--- wp-content/themes/shinbudokai/sidebar.php
+++ wp-content/themes/shinbudokai/sidebar.php
@@ -7,13 +7,25 @@
  * @package shinbudokai
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( !is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
+
 ?>
 	<!-- Default Sidebar -->
 	<aside id="secondary" class="widget-area" role="complementary">
+
+    <?php
+        $customSidebar = get_field('sidebar');
+        if ( !empty($customSidebar) ) :
+          echo $customSidebar;
+
+        else :
+    ?>
 		<div class="sidebar schedule">
 			<?php dynamic_sidebar(); ?>
 		</div>
+    <?php
+        endif;
+    ?>
 	</aside><!-- #secondary -->
